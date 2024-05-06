26879f769b1e44ce855fa72f1fc99afcc629c179
Ufkcnkmc <c.ufuk.kumcu@gmail.com>
2024-05-06 16:35:30 +0300

    project created

---
+++ src/test/java/com/MoneyTrasferSystem/MoneyTransferSystem/MoneyTransferSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MoneyTrasferSystem.MoneyTransferSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTransferSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
